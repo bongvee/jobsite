5f601fccfad2a25cc5f615d2f74355f936062c23
bongvee <bongvelasco.ph@gmail.com>
2023-06-09 01:30:58 +0800

    lesson 16 commit

--- jobs_app/urls.py
+++ jobs_app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('jobs/', views.readAllJobs, name='list_jobs'),
     path('jobs/<str:pk>/', views.readAJob, name='read_job'),
 
-    path('jobs/new/', views.createAJob, name='create_job'),
+    path('jobs/create/', views.createAJob, name='create_job'),
     path('jobs/<str:pk>/update/', views.updateAJob, name='update_job'),
     path('jobs/<str:pk>/delete/', views.deleteAJob, name='delete_job'),
 ]
